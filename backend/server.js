9a4f4acfde6d8e43669c6b704d9c8153e39efc5a
Alexxvr <129376967+Alexxvr@users.noreply.github.com>
2025-05-18 18:56:01 +0200

    Estructura

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// backend/server.js
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor backend funcionando!");
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
+});
